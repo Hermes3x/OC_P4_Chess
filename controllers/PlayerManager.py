2d990dbecbead4d0ab263989a7a43cb1379a7017
Hermes3x <jonathan.zvara@gmail.com>
2026-01-13 17:59:55 +0100

    Day 2

--- controllers/PlayerManager.py
+++ controllers/PlayerManager.py
@@ -1,49 +1,73 @@
 import random
 from models.players import Player
 
+
 class PlayerManager:
     def __init__(self):
         self.players_list = []
         self.pairs = []
 
-    def add_player(self, player):
-        self.players_list.append(player)
+    def create_player(self, last_name,
+                      first_name,
+                      birth_date,
+                      national_chess_id,
+                      ):
+        if len(self.players_list) == 0:
+            player_id = 1
+        else:
+            player_id = self.players_list[-1].player_id + 1
 
-    def shuffle_players(self):
-        random.shuffle(self.players_list)
+        new_player = Player(last_name,
+                            first_name,
+                            birth_date,
+                            national_chess_id,
+                            player_id)
 
-    def pairing_players(self):
-        self.pairs = []
-        if len(self.players_list) % 2 == 0:
-            for i in range(0, len(self.players_list), 2):
-                pair = (self.players_list[i], self.players_list[i + 1])
-                self.pairs.append(pair)
+        self.players_list.append(new_player)
 
-        else: 
-            print('il manque un joueur pour former un match supplémentaire')
+        return print(new_player)
+
+
+    # def add_player_to_tournament(self, player):
+    #     self.players_list.append(player)
+
+    # def shuffle_players(self):
+    #     random.shuffle(self.players_list)
+
+    # def pairing_players(self):
+    #     self.pairs = []
+    #     if len(self.players_list) % 2 == 0:
+    #         for i in range(0, len(self.players_list), 2):
+    #             pair = (self.players_list[i], self.players_list[i + 1])
+    #             self.pairs.append(pair)
+
+    #     else: 
+    #         print('il manque un joueur pour former un match supplémentaire')
                      
-    def __str__(self):
-        players = [p.first_name for p in self.players_list]
-        pairs_str = [f'{p1.first_name} vs {p2.first_name}'
-                     for (p1, p2) in self.pairs]
+    # def __str__(self):
+    #     players = [p.first_name for p in self.players_list]
+    #     pairs_str = [f'{p1.first_name} vs {p2.first_name}'
+    #                  for (p1, p2) in self.pairs]
 
-        return f"Players: {players} | Pairs: {pairs_str}"
+    #     return f"Players: {players} | Pairs: {pairs_str}"
 
-player1 = Player('Edouard', 'Plastik', '5/12/1963', 'AN123456')
-player2 = Player('Gerard', 'Contemporain', '12/05/1936', 'NA654321')
-player3 = Player('Gaspard', 'Chitect', '3/09/1393', 'BC4657981')
-player4 = Player('Baltasar', 'Delatable', '25/12/0003', 'CB649785')
 
 manager = PlayerManager()
-manager.add_player(player1)
-manager.add_player(player2)
-manager.add_player(player3)
-manager.add_player(player4)
+# manager.add_player(player1)
+# manager.add_player(player2)
+# manager.add_player(player3)
+# manager.add_player(player4)
 
-print(manager)
+manager.create_player('Edouard', 'Plastik', '5/12/1963', 'AN123456')
+manager.create_player('Gerard', 'Contemporain', '12/05/1936', 'NA654321')
+manager.create_player('Gaspard', 'Chitect', '3/09/1393', 'BC4657981')
+manager.create_player('Baltasar', 'Delatable', '25/12/0003', 'CB649785')
 
-manager.shuffle_players()
-print(manager)
 
-manager.pairing_players()
-print(manager)
+# print(manager)
+
+# manager.shuffle_players()
+# print(manager)
+
+# manager.pairing_players()
+# print(manager)
--- models/tournament.py
+++ models/tournament.py
@@ -1,3 +1,5 @@
+#round = 4
+
 class Tournament:
     def __init__(self,
                  name,
@@ -9,35 +11,12 @@ class Tournament:
         self.place = place
         self.begin_date = begin_date
         self.end_date = end_date
-        self.round = 4
+        self.round = round
         self.tournament_nomber = tournament_nomber
         self.round_list = []
-        self.players_list = []
+        self.players_list = {} #joueur + son score + [anciens adversaires]
         self.description = " "
 
     def __str__(self):
         return (f'{self.name} '
                 f'{self.place}')
-
-
-
-
-
-
-tn1 = Tournament('Coco', 'Tatouine', 'A long time ago', '01/12/-4768', '')
-
-tn1.add_player(player1)
-tn1.add_player(player2)
-tn1.add_player(player3)
-tn1.add_player(player4)
-
-print(tn1)
-
-print("\n--- OREDRE INITIAL ---")
-for player in tn1.players_list:
-    print(player)
-
-shuffled_players = tn1.shuffle_palyers()
-print("\n --- ORDRE APRES MELANGE ---")
-for player in tn1.players_list:
-    print(player)
