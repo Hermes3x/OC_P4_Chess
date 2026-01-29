a70c43724c3f1182f74255dcdab01bbfc51e3c7b
Hermes3x <jonathan.zvara@gmail.com>
2026-01-29 16:27:46 +0100

    JSON 99%

--- controllers/database_controller.py
+++ controllers/database_controller.py
@@ -1,4 +1,5 @@
 from models.player import Player
+from models.tournament import Tournament
 import json
 import os
 
@@ -35,3 +36,79 @@ class DatabaseController:
 
         print(f"📂 Chargement réussi : {len(loaded_players)} joueurs récupérés.")
         return loaded_players
+    
+    def save_tournament_to_json(self, tournament):
+        clean_name = tournament.name.replace(" ", "_").lower()
+        filename = f"data/tournaments/{clean_name}.json"
+
+        data = tournament.to_dict()
+
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open(filename, 'w') as file:
+            json.dump(data, file, indent=4)
+        
+        print(f"💾 Tournoi sauvegardé : {filename}")
+    
+    def load_tournament(self, filename):
+        if not os.path.exists(filename):
+            print(f"❌ Fichier introuvable : {filename}")
+            return None
+        
+        with open(filename, 'r') as file:
+            data = json.load(file)
+
+        all_players = self.load_players_from_json()
+        players_map = {p.national_chess_id: p for p in all_players}
+
+        tournament = Tournament(
+            name=data['name'],
+            place=data['place'],
+            date=data['date'],
+            rounds_qty=data['round_qty'],
+            note=data['note']
+        )
+
+        #  tournament.id = data['id']
+
+        for player_id in data['players_ids']:
+            if player_id in players_map:
+                tournament.add_player(players_map[player_id])
+            else:
+                print(f"⚠️ Joueur {player_id} introuvable dans la base globale !")
+
+        self._load_rounds(tournament, data['rounds_list'], players_map)
+
+        print(f"✅ Tournoi '{tournament.name}' chargé avec succès !")
+        return tournament
+    
+    def _load_rounds(seld, tournament, rounds_data, players_map):
+        from models.round import Round
+        from models.match import Match
+
+        for round_dict in rounds_data:
+            round_obj = Round(tournament)
+            round_obj.rounds_id = int(round_dict['round_id'].split(' ')[1])
+            round_obj.start_date = round_dict['start_date']
+            round_obj.end_date = round_dict['end_date']
+
+            for match_data in round_dict['matchs']:
+                id1, score1 = match_data[0]
+                id2, score2 = match_data[1]
+
+                p1 = players_map.get(id1)
+                p2 = players_map.get(id2)
+
+                if p1 and p2:
+                    match_obj = Match(p1, p2)
+                    match_obj.player1_score = score1
+                    match_obj.player2_score = score2
+                    # match_obj.player1_color = color1
+                    # match_obj.player2_color = color2
+
+                    p1.score += score1
+                    p2.score += score2
+
+                    round_obj.matchs.append(match_obj)
+
+            tournament.rounds_list.append(round_obj)
+            tournament.actual_round = round_obj.rounds_id
--- main.py
+++ main.py
@@ -4,42 +4,28 @@ from models.tournament import Tournament
 import random
 import pprint
 
-
-tournament1 = Tournament(
-    name="Tournoi OpenClassrooms",
-    place="Paris",
-    date="27/01/2026",
-    rounds_qty=4,
-    note="Projet 4")
-
 db_controller = DatabaseController()
-players_from_json = db_controller.load_players_from_json()
+loaded_tournament = db_controller.load_tournament("data/tournaments/tournoi_openclassrooms.json")
 
-for player in players_from_json:
-    tournament1.add_player(player)
+print(f'Tournoi chargé: {loaded_tournament}')
+print(f'Joueurs: {len(loaded_tournament.tournament_players)}')
+print(f'Score de {loaded_tournament.tournament_players[0].last_name} ; {loaded_tournament.tournament_players[0].score}')
 
-print(f'{len(players_from_json)} joueurs ont été ajoutés au tournoi')
 
-round1 = tournament1.create_round()
-for match in round1.matchs:
-    p1, p2 = match.players_pair
-    match.score(random.choice([p1, p2, None]))
-    print(f'{match}')
+# tournament1 = Tournament(
+#     name="Tournoi OpenClassrooms",
+#     place="Paris",
+#     date="27/01/2026",
+#     rounds_qty=4,
+#     note="Projet 4")
 
-round1.close_round()
+# db_controller = DatabaseController()
+# players_from_json = db_controller.load_players_from_json()
 
-round2 = tournament1.create_round()
-for match in round2.matchs:
-    p1, p2 = match.players_pair
-    match.score(random.choice([p1, p2, None]))
-    print(f'Résultats \n{match}')
+# for player in players_from_json:
+#     tournament1.add_player(player)
 
-round2.close_round()
-
-print("\n--- TEST EXPORT COMPLET ---")
-full_dict = tournament1.to_dict()
-pprint.pprint(full_dict)
-
+# print(f'{len(players_from_json)} joueurs ont été ajoutés au tournoi')
 
 # round1 = tournament1.create_round()
 # print('\nmatchs du round 1: \n')
@@ -48,11 +34,11 @@ pprint.pprint(full_dict)
 #     match.score(random.choice([p1, p2, None]))
 #     print(f'{match}')
 
+# round1.close_round()
 
 # tournament1.tournament_players.sort(key=lambda p: (-p.score, p.last_name))
 # for player in tournament1.tournament_players:
 #     print(f'{player}')
-
 
 # round2 = tournament1.create_round()
 # print('\nmatchs du round 2: ')
@@ -60,6 +46,8 @@ pprint.pprint(full_dict)
 #     p1, p2 = match.players_pair
 #     match.score(random.choice([p1, p2, None]))
 #     print(f'{match}')
+
+# round2.close_round()
 
 # tournament1.tournament_players.sort(key=lambda p: (-p.score, p.last_name))
 # for player in tournament1.tournament_players:
@@ -72,6 +60,8 @@ pprint.pprint(full_dict)
 #     match.score(random.choice([p1, p2, None]))
 #     print(f'{match}')
 
+# round3.close_round()
+
 # tournament1.tournament_players.sort(key=lambda p: (-p.score, p.last_name))
 # for player in tournament1.tournament_players:
 #     print(f'{player}')
@@ -83,10 +73,15 @@ pprint.pprint(full_dict)
 #     match.score(random.choice([p1, p2, None]))
 #     print(f'{match}')
 
+# round4.close_round()
+
 # print('\n🏆 CLASSEMENT FINAL DU TOURNOI 🏆')
 # tournament1.tournament_players.sort(key=lambda p: (-p.score, p.last_name))
 # for i, player in enumerate(tournament1.tournament_players, 1):
 #     print(f'{i}. {player.first_name} {player.last_name} - {player.score} pts')
+
+
+# db_controller.save_tournament_to_json(tournament1)
 
 
 # for pair in tournament1.rounds_list:
